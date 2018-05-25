8d7d00d8d54b435ba99f7522793dc79e449471d6
Cristhian Traversi <crist.trav@gmail.com>
2018-05-25 17:56:59 -0400

    Modificada ventana principal: -Agregado MenuBar -Agregado TabPane -Agregado Metodo generico para cargar archivos .FXML

---
+++ src/main/java/edu/itssnpp/itsmarket/VentanaPrincipalController.java
@@ -0,0 +1,54 @@
+package edu.itssnpp.itsmarket;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+
+public class VentanaPrincipalController implements Initializable {
+
+    private static final Logger LOG = Logger.getLogger(VentanaPrincipalController.class.getName());
+    
+    
+    private Label label;
+    @FXML
+    private TabPane tabPane;
+    
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hola mundo!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    private void cargarModulo(String direccionFXML, String tituloPestania) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            AnchorPane root = FXMLLoader.load(getClass().getResource(direccionFXML));
+            Tab t=new Tab();
+            t.setText(tituloPestania);
+            t.setContent(root);
+            this.tabPane.getTabs().add(t);
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, "Error al cargar modulo", ex);
+            Alert errDlg=new Alert(Alert.AlertType.ERROR);
+            errDlg.setTitle("Error al cargar módulo");
+            errDlg.setHeaderText("Error al cargar módulo: '"+tituloPestania+"'. Archivo: '"+direccionFXML+"'.");
+            errDlg.setContentText(ex.getMessage());
+            errDlg.showAndWait();
+        }
+    }
+}

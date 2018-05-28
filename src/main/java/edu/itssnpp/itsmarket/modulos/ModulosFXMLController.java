1062e5df76a061f717057536568fa58a88cc8cd4
estela <estela@prg02-pc01>
2018-05-28 14:18:55 -0400

    creacion de fxml y controller de modulos

---
+++ src/main/java/edu/itssnpp/itsmarket/modulos/ModulosFXMLController.java
@@ -0,0 +1,17 @@
+
+package edu.itssnpp.itsmarket.modulos;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class ModulosFXMLController implements Initializable {
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       
+    }    
+    
+}

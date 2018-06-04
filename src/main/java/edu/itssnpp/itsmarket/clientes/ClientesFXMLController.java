b4898722c6667d8b413e7f3343222895ad0053bd
fredy <fredy@prg02-pc02>
2018-06-04 13:48:13 -0400

    diseño de la ventana de Clientes

--- src/main/java/edu/itssnpp/itsmarket/clientes/ClientesFXMLController.java
+++ src/main/java/edu/itssnpp/itsmarket/clientes/ClientesFXMLController.java
@@ -2,7 +2,12 @@ package edu.itssnpp.itsmarket.clientes;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -11,6 +16,27 @@ import javafx.fxml.Initializable;
  */
 public class ClientesFXMLController implements Initializable {
 
+    @FXML
+    private TextField nombre;
+    @FXML
+    private TextField apellido;
+    @FXML
+    private TextField cinro;
+    @FXML
+    private TextField dvruc;
+    @FXML
+    private TextField telefono;
+    @FXML
+    private TextField direccion;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField idcliente;
+    @FXML
+    private Button button;
+    @FXML
+    private ComboBox<?> ciudad;
+
     /**
      * Initializes the controller class.
      */
@@ -18,5 +44,9 @@ public class ClientesFXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void agregar(ActionEvent event) {
+    }
     
 }
